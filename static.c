e7d35bc21f4b34a57b684636a4f461d3e526c554
Mohammed H. Al-Sayegh <40126750+mohammedalsayegh@users.noreply.github.com>
2019-04-09 16:06:24 -0400

    Look up table
    This code create look up table array in C base on Fs and block size

---
+++ static.c
@@ -0,0 +1,25 @@
+//The full program, say static.c, would look like this: 
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include <math.h>
+
+#define N 200 
+#define pi 3.14159265358979323846
+
+int main (void) 
+{ 
+	float t[N], f[N];
+	int i;
+	float freq = 12000;
+
+	for (i = 0; i < N; i++)
+	{ 
+		t[i] = (float)(i) / (((float)(N) - 1.0)*1000);
+		f[i] = sin(2 * pi * freq * t[i]);
+		//printf("The value t[%d] %f\n",i,t[i]);
+		printf("The value f[%d] %f\n",i,f[i]);
+	}
+
+	return EXIT_SUCCESS; 
+}
