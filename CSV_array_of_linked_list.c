b19b88b11b3415ccf311a11e85441acdfe258844
Mohammed Alsayegh <mohammed_89_12@yahoo.com>
2023-03-30 03:09:46 +0300

    This is project made to load very large CSV files to a linked list of array

---
+++ CSV_array_of_linked_list.c
@@ -0,0 +1,59 @@
+// Namd:Mohammed Alsayegh
+// This program load super large CSV file to buffer array of structs
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct record_t{
+    unsigned int number;
+    char seq[25];
+    struct record_t *next;
+}*head;
+
+int main(void) {
+    //file var
+    FILE *fp;
+    
+    //time var
+    time_t start,end;
+    double dif;
+
+    //struct array var
+    int size,si;
+    struct record_t *temp;
+    size=98000;
+    si=1024;
+
+    //open the csv file
+    fp = fopen("data4.csv", "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error reading file\n");
+        return 1;
+    }
+    
+    struct record_t *A[size];
+    time (&start);
+    for(int i=0;i<size;i++){
+        head = (struct record_t *)malloc(sizeof(struct record_t));
+        fscanf(fp, "%d,%25s", &head->number, head->seq);
+        head -> next = NULL;
+        temp = head;
+        for(int j=1;j<si;j++){
+            struct record_t *new = (struct record_t *)malloc(sizeof(struct record_t));
+            fscanf(fp, "%d,%25s", &new->number, new->seq);
+            new -> next = NULL;
+            temp -> next = new;
+            temp = new;
+        }
+        A[i] = head;
+        if(!(i%1000)) printf("block number: %d\n", i);
+    }
+    time (&end);
+    dif = difftime (end,start);
+    printf ("malloc took %.2lf seconds to run.\n", dif);
+
+    printf("no Errors\n");
+    fclose(fp);
+    return 0;
+}
