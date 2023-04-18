cd6e25159c8ccb567ee04271eb8344d8b6875f5e
Mohammed Alsayegh <mohammed_89_12@yahoo.com>
2023-04-18 11:49:09 +0300

    commenting and adjusting

--- parsing_3bits_in_8bits_buffer.c
+++ parsing_3bits_in_8bits_buffer.c
@@ -1,25 +1,29 @@
 // Name: Mohammed Alsayegh
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 
 #define size_parsing 3
 
-int main() 
-{
+int main() {
+
     uint8_t arr_n[] = {0b101, 0b001, 0b111, 0b001, 0b111, 0b000, 0b001, 0b010, 0b001, 0b011, 0b111, 0b001};
 
     int i;
     unsigned int len = sizeof(arr_n);
-    uint8_t buf=0;
-    uint8_t bitwise_location = 8;
-    uint8_t bit_left=0;
-    uint8_t bit_fill_left=0;
-    uint8_t buf_cycle=0;
-    bool buf_full=false;
 
-    for (i = 0; i < len; ++i) 
-    {
+    uint8_t buf = 0;
+    uint8_t bitwise_location = 8;
+    uint8_t bit_left = 0;
+    uint8_t bit_fill_left = 0;
+    uint8_t buf_cycle = 0;
+    bool buf_full = false;
+
+    // Loop through each byte of the array
+    for (i = 0; i < len; ++i) {
+
+        // Update the buffer if the bit parsed size is greater than the remaining space
         if (bitwise_location < size_parsing) {
             buf_full = true;
             buf_cycle++;
@@ -32,16 +36,18 @@ int main()
             buf = 0;
             bitwise_location = 8 - (size_parsing - bit_left);
             if (!(buf_cycle % size_parsing)) buf_cycle = 0;
+
         } else {
+
             if (buf_cycle < size_parsing && buf_full == true) buf |= arr_n[i - 1] << (bitwise_location);
-            buf_full = false;
-            bitwise_location = bitwise_location - size_parsing;
+            bitwise_location -= size_parsing;
             buf |= arr_n[i] << (bitwise_location);
+            buf_full = false;
         }
     }
 
-    if (buf_cycle % size_parsing) 
-    {
+    // If there are any remaining bits in the last byte, shift them into the buffer
+    if (buf_cycle % size_parsing) {
         --i;
         buf |= arr_n[i] << (bitwise_location);
         printf("Value: %hhu\n", buf);
