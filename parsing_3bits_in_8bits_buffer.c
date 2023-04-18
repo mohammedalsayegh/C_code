a1d67706de33dfeddbb71e274d41eb27389e2b20
Mohammed Alsayegh <mohammed_89_12@yahoo.com>
2023-04-18 05:25:34 +0300

    make it print uint8_t buf value instead of binary

--- parsing_3bits_in_8bits_buffer.c
+++ parsing_3bits_in_8bits_buffer.c
@@ -5,45 +5,34 @@
 
 #define size_parsing 3
 
-void print_binary(uint8_t n)
+int main() 
 {
-    unsigned i;
-    for (i = 1 << 7; i > 0; i = i / 2)
-        (n & i) ? printf("1") : printf("0");
-    printf("\n");
-}
-
-int main() {
-
     uint8_t arr_n[] = {0b101, 0b001, 0b111, 0b001, 0b111, 0b000, 0b001, 0b010, 0b001, 0b011, 0b111, 0b001};
 
-    unsigned int len = sizeof(arr_n);
-
-    printf("len %u\n", len);
-    uint8_t buf;
-
     int i;
+    unsigned int len = sizeof(arr_n);
+    uint8_t buf=0;
     uint8_t bitwise_location = 8;
     uint8_t bit_left=0;
     uint8_t bit_fill_left=0;
     uint8_t buf_cycle=0;
     bool buf_full=false;
 
-    for (i = 0; i < len; ++i) {
+    for (i = 0; i < len; ++i) 
+    {
         if (bitwise_location < size_parsing) {
-
             buf_full = true;
             buf_cycle++;
             bit_left = bitwise_location;
             bit_fill_left = arr_n[i] >> (size_parsing - bit_left);
             buf |= bit_fill_left;
-            printf("complete: buf[%u]: ",i);
-            print_binary(buf);
+
+            printf("Value: %hhu\n", buf);
+
             buf = 0;
             bitwise_location = 8 - (size_parsing - bit_left);
             if (!(buf_cycle % size_parsing)) buf_cycle = 0;
         } else {
-
             if (buf_cycle < size_parsing && buf_full == true) buf |= arr_n[i - 1] << (bitwise_location);
             buf_full = false;
             bitwise_location = bitwise_location - size_parsing;
@@ -51,16 +40,11 @@ int main() {
         }
     }
 
-    if (!(buf_cycle % size_parsing)) {
-        printf("complete!\n");
-        return 0;
-    }
-    else {
+    if (buf_cycle % size_parsing) 
+    {
         --i;
         buf |= arr_n[i] << (bitwise_location);
-        printf("buf[%u]: ",i);
-        print_binary(buf);
-        printf("complete!\n");
+        printf("Value: %hhu\n", buf);
     }  
 
     return 0;
